3fdbf77224debd1652bb203505fc4e586845a9d9
a.cobo.2020 <a.cobo.2020@alumnos.urjc.es>
2022-02-11 10:16:40 +0100

    bumandgo v2.0.0
    Signed-off-by: a.cobo.2020 <a.cobo.2020@alumnos.urjc.es>

--- src/fsm_bump_go/BumpGo.cpp
+++ src/fsm_bump_go/BumpGo.cpp
@@ -1,4 +1,4 @@
-// Copyright 2022 Intelligent Robotics Lab
+// Copyright 2019 Intelligent Robotics Lab
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,53 +12,50 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "fsm_bump_go/BumpGo.h"
+#include "ros/ros.h"
 
 #include "kobuki_msgs/BumperEvent.h"
 #include "geometry_msgs/Twist.h"
 
-#include "ros/ros.h"
+#define TURNING_TIME 5.0
+#define BACKING_TIME 3.0
 
-namespace fsm_bump_go
+class BumpGo
 {
-
-BumpGo::BumpGo()
-: state_(GOING_FORWARD),
-  pressed_(false)
-{
-  // sub_bumber_ = n_.subscribe(...);
-  // pub_vel_ = n_.advertise<...>(...)
-}
-
-void
-BumpGo::bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
-{
-  // pressed_ = (...);
-  //  ...
-}
-
-void
-BumpGo::step()
-{
-  geometry_msgs::Twist cmd;
-
-  switch (state_)
+public:
+  BumpGo(): state_(GOING_FORWARD), pressed_(false)
   {
-    case GOING_FORWARD:
-      // cmd.linear.x = ...;
-      // cmd.angular.z = ...;
+    sub_bumber_ = n_.subscribe("mobile_base/events/bumper", 1, &BumpGo::bumperCallback,this);
+    pub_vel_ = n_.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1);
+  }
 
-      if (pressed_)ç
+  void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
+  {
+    pressed_ = msg->state;
+
+    //  ...
+  }
+
+  void step()
+  {
+    geometry_msgs::Twist cmd;
+
+    switch (state_)
+    {
+    case GOING_FORWARD:
+      cmd.linear.x = 0.3;
+      cmd.angular.z = 0.0;
+      if (pressed_)
       {
         press_ts_ = ros::Time::now();
         state_ = GOING_BACK;
         ROS_INFO("GOING_FORWARD -> GOING_BACK");
       }
-
       break;
+
     case GOING_BACK:
-      // cmd.linear.x = ...;
-      // cmd.angular.z = ...;
+      cmd.linear.x = -0.3;
+      cmd.angular.z = 0.0;
 
       if ((ros::Time::now() - press_ts_).toSec() > BACKING_TIME )
       {
@@ -66,12 +63,11 @@ BumpGo::step()
         state_ = TURNING;
         ROS_INFO("GOING_BACK -> TURNING");
       }
-
       break;
-    case TURNING:
-      // cmd.linear.x = ...;
-      // cmd.angular.z = ...;
 
+    case TURNING:
+      cmd.linear.x = 0.0;
+      cmd.angular.z = 0.3;
       if ((ros::Time::now()-turn_ts_).toSec() > TURNING_TIME )
       {
         state_ = GOING_FORWARD;
@@ -80,7 +76,43 @@ BumpGo::step()
       break;
     }
 
-    // pub_vel_.publish(...);
-}
+    pub_vel_.publish(cmd);
+  }
 
-}  // namespace fsm_bump_go
+private:
+  ros::NodeHandle n_;
+
+  static const int GOING_FORWARD   = 0;
+  static const int GOING_BACK   = 1;
+  static const int TURNING     = 2;
+
+  int state_;
+
+  bool pressed_;
+
+  ros::Time press_ts_;
+  ros::Time turn_ts_;
+
+  ros::Subscriber sub_bumber_;
+  ros::Publisher pub_vel_;
+};
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "bumpgo");
+
+  BumpGo bumpgo;
+
+  ros::Rate loop_rate(20);
+
+  while (ros::ok())
+  {
+    bumpgo.step();
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
+  return 0;
+}
