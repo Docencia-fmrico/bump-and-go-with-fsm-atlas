ac0d0c30c72e27ed8d987c1671240251e6581609
Jose Miguel Jimenez Garcia <josemjim@f-l3202-pc05.aulas.etsit.urjc.es>
2022-02-12 11:01:55 +0100

    BumpGo_Advanced moved

--- src/fsm_bump_go/BumpGo_Advanced.cpp
+++ src/fsm_bump_go/BumpGo_Advanced.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "fsm_bump_go/BumpGo_Advanced.h"
+#include "BumpGo_Advanced.h"
 
 #include "kobuki_msgs/BumperEvent.h"
 #include "geometry_msgs/Twist.h"
