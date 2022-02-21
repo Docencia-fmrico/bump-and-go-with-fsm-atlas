27c9b9df13a46b203da7b54ca554873974a7ec40
Estefania Ochoa <e.ochoa.2020@alumnos.urjc.es>
2022-02-21 12:49:03 +0100

    callback laser modificado

--- src/fsm_bump_go/BumpGo_Advanced_Laser.cpp
+++ src/fsm_bump_go/BumpGo_Advanced_Laser.cpp
@@ -16,6 +16,8 @@
 
 #include "sensor_msgs/LaserScan.h"
 
+#define PI 3.14159265
+
 namespace fsm_bump_go
 {
 
@@ -28,28 +30,24 @@ BumpGo_Advanced_Laser::BumpGo_Advanced_Laser()
 void
 BumpGo_Advanced_Laser::laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-  float nearest_obs_d = msg->ranges[0]; 
-  int index = 0;
-  for (int i = 1; i < msg->ranges.size()-1; i++)
+  int n_reading = 1;  // number of laser readings 
+  
+  for (int i = 0; i < msg->ranges.size(); i++)
   {
-    if (msg->ranges[i] < nearest_obs_d && msg->ranges[i] > RANGE_MIN_DETECTED)
+    if (msg->ranges[i] < nearest_obs_d && msg->ranges[i] > RANGE_MIN_DETECTED && msg->ranges[i] < RANGE_MAX_DETECTED)
     {
-      nearest_obs_d = msg->ranges[i];
-      index = i;
+      float nearest_obs_d = msg->ranges[i];
+      n_reading = i+1;
     }
   }
-
-  detected_obs_ = nearest_obs_d > RANGE_MIN_DETECTED && nearest_obs_d < RANGE_MAX_DETECTED;
   
-  if (detected_obs_)
-  {
-    float angle_detected_obs_ = msg->angle_min + msg->angle_increment*index;
+  float angle_obs = msg->angle_min + msg->angle_increment*n_reading;
 
-    left_obstacle_ = angle_detected_obs_ < msg->angle_min/2 && angle_detected_obs_ > 0;
-    right_obstacle_ = angle_detected_obs_ < 0 && angle_detected_obs_ > msg->angle_max/2;
-  }
+  left_obstacle_ = angle_obs < PI/3 && angle_obs > 0;
+  right_obstacle_ = angle_obs < 0 && angle_obs > -PI/3;
+  detected_obs_ = left_obstacle_ || right_obstacle_;
 
-  //ROS_INFO("%d %d", left_obstacle_, right_obstacle_);
+  ROS_INFO("%d %d", left_obstacle_, right_obstacle_);
 }
 
 }  // namespace fsm_bump_go
